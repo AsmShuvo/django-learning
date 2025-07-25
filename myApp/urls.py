c646e75cb9df31a706158aa8094bc82330640734
AsmShuvo <hoffmanhulk99@gmail.com>
2025-07-25 16:03:56 +0600

    get list

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import testView  , timeView
+from .views import testView  , timeView, list_view
 
 urlpatterns = [
     path('test/', testView, name='test'),
     path("time/", timeView, name='time'),
+    path("", list_view, name='list'),
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 import datetime
+from .models import GeeksModel
 def testView(req):
     return HttpResponse("testing...")
 
@@ -8,4 +9,9 @@ def timeView(req):
     date = datetime.datetime.now()
     res = "Time is {}".format(date)
     return HttpResponse(res)
+
+def list_view (req):
+    context = {}
+    context["dataset"] = GeeksModel.objects.all()
+    return render(req, "list_view.html", context)
     
